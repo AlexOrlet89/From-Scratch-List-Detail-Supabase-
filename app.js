c1a4d3e207736a20c9c6c6fce13cd830f3403a33
Alex Orlet <AlexOrlet89@gmail.com>
2022-02-10 15:50:09 -0800

    renderAlbums returns <a>, appended to div in app.js, albums rendering

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import functions and grab DOM elements
 import { getAlbums } from './fetch-utils.js';
+import { renderAlbums } from './render-utils.js';
 // let state
 const discog = document.getElementById('Main-list');
 // set event listeners 
@@ -8,10 +9,13 @@ window.addEventListener('load', async() => {
     console.log(albums);
 
     for (let album of albums) {
-      const albumEl = renderAlbum(album);
-      //create a render function that is given an album's worth of data
+        const albumEl = renderAlbums(album);
+      //create a render function that is given an album's worth of data and returns a div to append to discog
+        console.log(albumEl);
+        discog.append(albumEl);
     }
 });
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
--- render-utils.js
+++ render-utils.js
@@ -1,3 +1,28 @@
-export function renderAlbums(cd) {
+export function renderAlbums(album) {
     
+    //this function is given a single object, in this case an album, we must create elements and define their contents with the values in our object. We have our HTML for our test, we will go ahead and create these elements based on that :)
+
+    const div = document.createElement('div');
+    const h3 = document.createElement('h3');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    const a = document.createElement('a');
+
+    // alright, now that we have our elements created, its time to see if our data is passing to us through our function's argument (cd). let's console log a key to see if we can get it's value.
+    // console.log(album.id);
+    // console.log(album.YearReleased);
+    console.log(album.Tracks.length);
+    console.log(album.Tracks.split(',').length);
+
+    h3.textContent = album.AlbumName;
+    img.src = `/assets/${album.id}.jpg`;
+    p.textContent = `${album.YearReleased}, ${album.Tracks.split(',').length} tracks`;
+    a.href = `/Album/?id=${album.id}`;
+    
+    div.append(h3, img, p);
+
+    a.append(div);
+
+    return a;
+
 }
