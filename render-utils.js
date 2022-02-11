497e5448535c8bc9235affc3360c1b878232bccc
Alex Orlet <AlexOrlet89@gmail.com>
2022-02-11 12:16:30 -0800

    tried many ways to pull an array from sb, ended up initializing column as text with array option on

--- render-utils.js
+++ render-utils.js
@@ -11,39 +11,52 @@ export function renderAlbums(album) {
     // alright, now that we have our elements created, its time to see if our data is passing to us through our function's argument (cd). let's console log a key to see if we can get it's value.
     // console.log(album.id);
     // console.log(album.YearReleased);
-    console.log(album.Tracks.length);
-    console.log(album.Tracks.split(',').length);
+    // console.log(album.Tracks.length);
+    // console.log(album.Tracks.split(',').length);
 
     h3.textContent = album.AlbumName;
     img.src = `/assets/${album.id}.jpg`;
-    p.textContent = `${album.YearReleased}, ${album.Tracks.split(',').length} tracks`;
+    p.textContent = `${album.YearReleased}, ${album.Tracks.length} tracks`;
     a.href = `/Album/?id=${album.id}`;
     
-    div.append(h3, img, p);
+    // console.log(album.Tracks.data);
 
-    a.append(div);
-
+    a.append(h3, img, p);
+    
+    div.append(a);
     return a;
+    
 }
 
 export function renderAlbum(album) {
-// console.log(album);   
+    console.log(album);   
     const div = document.createElement('div');
     const albumName = document.createElement('h2');
     const year = document.createElement('h4');
     const albumArt = document.createElement('img');
     const ol = document.createElement('ol');
+    const title = document.querySelector('title');
 
+    div.classList.add('album');
+    div.id = 'album';
+
+    title.textContent = album.AlbumName;
     albumName.textContent = album.AlbumName;
     year.textContent = album.YearReleased;
     albumArt.src = `/assets/${album.id}.jpg`;
 
-    for (let track of (album.Tracks.split(','))) {
+    console.log(album.Tracks);
+    
+    for (let track of (album.Tracks)) {
+        console.log(album.Tracks);
         const li = document.createElement('li');
-        li.textContent = track.replace('[', '').replace(']', '').replace(`'`, '').replace(`'`, '');
+        li.textContent = track;
         ol.append(li);
     }
+        
     div.append(albumName, year, albumArt, ol);
     return div;
 
+        
 }
+    
--- test/example.test.js
+++ test/example.test.js
@@ -1,32 +1,35 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { fourtetAlbums } from '../fourtet.js';
+import { renderAlbum, renderAlbums } from '../render-utils.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = '<div class="album"><h3>Morning / Evening</h3><img src="assets/1.jpg" class="Album-art"><p>2015, 2 tracks</p></div>';
+// test('renderAlbums should render an album as a div with a class of album', (expect) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const expected = '<div class="album"><h3>Morning / Evening</h3><img src="assets/1.jpg" class="Album-art"><p>2015, 2 tracks</p></div>';
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actual = renderAlbums(fourtetAlbums[0]);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
-});
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expect.equal(actual.outerHTML, expected);
+// });
 
-test('time to test a function', (expect) => {
+test('renderAlbum should render an album as a div', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = `<div class="album" id="album"><h2>Morning / Evening</h2><h4>2015</h4><img src="/assets/1.jpg"><ol><li>Morning Side</li><li>Evening Side</li></ol></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
-
+    const actual = renderAlbum({ id: 1, AlbumName: 'Morning / Evening', YearReleased: '2015', Tracks:['Morning Side', 'Evening Side'] });
+    console.log('dog');
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
